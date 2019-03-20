55ac2926288a0c864a9fc9685974dc7c1b79c0a8
nmhung1210 <nmhung1210@gmail.com>
2019-03-21 00:33:59 +0700

    New released

---
+++ precache-manifest.b238910142a56be6208c7d43494f4518.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bd57e020e0602b2f439e",
+    "url": "/static/js/main.9f164ed2.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "bd57e020e0602b2f439e",
+    "url": "/static/css/main.a635301f.chunk.css"
+  },
+  {
+    "revision": "16eabc20c378d9ccb798ad96ec2cab41",
+    "url": "/index.html"
+  }
+];
